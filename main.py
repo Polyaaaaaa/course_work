6973cb428af1d56830584eb937426cfb0050add4
Polyaaaaaa <polinasatrajkina@gmail.com>
2024-07-10 00:53:23 +0300

    final movements

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ from src.services import get_operations_dict
 from src.views import get_json_answer
 
 if __name__ == "__main__":
+
     """основная функция"""
     date = input("Введите пожалуйста время, по которому хотите запуск программы (формат YYYY.MM.DD HH:MM:SS)\n")
     print(get_json_answer(date))
-    transactions = get_operations_dict(os.path.join("data", "operations.xls"))
-    # transactions = get_operations_dict(os.path.join("..", "data", "operations.xls"))
+    # transactions = get_operations_dict(os.path.join("data", "operations.xls"))
+    data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
+    transactions = get_operations_dict(os.path.join(data_dir, "operations.xls"))
 
     user_input = input(
         "Хотите проанализировать ваши траты по дням недели за последние три месяца " "(от переданной даты)? Да/Нет\n"
